79148a62b9b9ad26bf320b9118ef41aecfc1b54a
Matheus Schneider <matheus.ssm@gmail.com>
2022-02-07 16:43:53 -0300

    add: function to add new firestore document

--- src/components/FormBox/index.tsx
+++ src/components/FormBox/index.tsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 import { Container } from './styles';
 import { ButtonIcon } from '../ButtonIcon';
 import { Input } from '../Input';
 
 export function FormBox() {
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  async function handleAddProduct() {
+    firestore()
+      .collection('products')
+      .add({
+        description,
+        amount,
+        done: false,
+      })
+      .then(() => {
+        Alert.alert('Produto adicionado com sucesso!');
+      })
+      .catch((error) => console.error(error));
+  }
+
   return (
     <Container>
       <Input
-        placeholder="Nome do produto"
-        size="medium"
+        placeholder='Nome do produto'
+        size='medium'
+        onChangeText={setDescription}
       />
 
       <Input
-        placeholder="0"
-        keyboardType="numeric"
-        size="small"
+        placeholder='0'
+        keyboardType='numeric'
+        size='small'
         style={{ marginHorizontal: 8 }}
+        onChangeText={(value) => setAmount(Number(value))}
       />
 
       <ButtonIcon
         size='large'
-        icon="add-shopping-cart"
-        onPress={() => { }}
+        icon='add-shopping-cart'
+        onPress={handleAddProduct}
       />
     </Container>
   );
