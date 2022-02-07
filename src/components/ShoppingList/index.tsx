ae71e39fd33364a5d43ce62cd916fb9f47e4426f
Matheus Schneider <matheus.ssm@gmail.com>
2022-02-07 16:48:48 -0300

    update: removed fake products

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -4,15 +4,13 @@ import { FlatList } from 'react-native';
 import { styles } from './styles';
 import { Product, ProductProps } from '../Product';
 
-import { shoppingListExample } from '../../utils/shopping.list.data';
-
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+  const [products, setProducts] = useState<ProductProps[]>([]);
 
   return (
     <FlatList
       data={products}
-      keyExtractor={item => item.id}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => <Product data={item} />}
       showsVerticalScrollIndicator={false}
       style={styles.list}
