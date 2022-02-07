b4647239cff0273af3dfa963de83304f0a7e993b
Matheus Schneider <matheus.ssm@gmail.com>
2022-02-07 17:07:34 -0300

    update: sorted products lsit

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -28,6 +28,7 @@ export function ShoppingList() {
   useEffect(() => {
     const subscribe = firestore()
       .collection('products')
+      .orderBy('description')
       .onSnapshot((querySnapshot) => {
         const data = querySnapshot.docs.map((doc) => {
           return {
