2d0d90c006a217ed2aca4a5ebe0b4ec3759bf454
Matheus Schneider <matheus.ssm@gmail.com>
2022-02-08 18:17:27 -0300

    add: function to demonstrate user creation

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import auth from '@react-native-firebase/auth';
 
 import { Container, Account, Title, Subtitle } from './styles';
 import { ButtonText } from '../../components/ButtonText';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
+import { Alert } from 'react-native';
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   async function handleSignInAnonymously() {
     const { user } = await auth().signInAnonymously();
     console.log(user);
+  }
+
+  function handleCreateUserAccount() {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        Alert.alert('Usuário criado com sucesso');
+      })
+      .catch((error) => {
+        Alert.alert('Oops...', 'Erro ao criar usuário');
+        console.error(error);
+      });
   }
 
   return (
@@ -17,15 +33,22 @@ export function SignIn() {
       <Title>MyShopping</Title>
       <Subtitle>monte sua lista de compra te ajudar nas compras</Subtitle>
 
-      <Input placeholder='e-mail' keyboardType='email-address' />
+      <Input
+        placeholder='e-mail'
+        keyboardType='email-address'
+        onChangeText={setEmail}
+      />
 
-      <Input placeholder='senha' secureTextEntry />
+      <Input placeholder='senha' secureTextEntry onChangeText={setPassword} />
 
       <Button title='Entrar' onPress={() => {}} />
 
       <Account>
         <ButtonText title='Recuperar senha' onPress={() => {}} />
-        <ButtonText title='Criar minha conta' onPress={() => {}} />
+        <ButtonText
+          title='Criar minha conta'
+          onPress={handleCreateUserAccount}
+        />
       </Account>
     </Container>
   );
